da46c5057b522bd833a5c9f02b49c54a845d38c0
David Steinacher <david.steinacher@bitmovin.com>
2021-02-19 13:49:16 +0100

    add empty example

---
+++ Example/CoreRecordExample/CoreRecordExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreRecordExample
+//
+//  Created by David Steinacher on 19.02.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
